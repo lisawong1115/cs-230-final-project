00a160144a5eb9aaca05c761b63939c97c002f14
huihanlhh <huihanli831@gmail.com>
2017-12-08 22:26:47 -0500

    First commit to GUI

---
+++ GUI/CatifyGUI.java
@@ -0,0 +1,13 @@
+/** 
+* CatifyGUI.java
+* 
+* @author Lisa Huang (rhuang2), Huihan Li (hli3) and Tina Zhang (yzhang16)
+* @since 12-08-2017
+*/
+import javax.swing.*;
+
+public class CatifyGUI {
+
+  public static void main (String[] args) {
+  }
+}
---
+++ GUI/GamePanel.java
@@ -0,0 +1,14 @@
+/** 
+* GamePanel.java
+* 
+* @author Lisa Huang (rhuang2), Huihan Li (hli3) and Tina Zhang (yzhang16)
+* @since 12-08-2017
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class GamePanel extends JPanel {
+}
