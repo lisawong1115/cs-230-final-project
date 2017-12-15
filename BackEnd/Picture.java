65b85dc62cb8d3f2d0d7c52dd05658f01f08a769
Tinazzz <yzhang16@wellesley.edu>
2017-12-15 08:18:36 -0500

    Add files via upload

--- BackEnd/Picture.java
+++ BackEnd/Picture.java
@@ -2,6 +2,42 @@
  * Picture.java
  * */
 
-public class Picture extends Comparable<Picture>{
+public class Picture implements Comparable<Picture>{
+  // Instance variable 
+  String picFileName;
+  int picID; 
   
+  public Picture (int picID, String picFileName){
+    this.picFileName = picFileName; 
+    this.picID = picID; 
+  }
+  
+  public int getID(){
+    return picID;
+  }
+  
+  public String getFileName(){
+    return picFileName;
+  }
+  
+  //return -1, 0, 1 
+  public int compareTo(Picture Pic2){
+    if(this.getID()>Pic2.getID()){
+      return 1; 
+    }
+    else if(this.getID()==Pic2.getID()){
+      return 0;
+    }
+    else{
+      return -1;
+    }
+  }
+  
+  //main method to test 
+  public static void main (String args[]){
+    Picture testpic = new Picture (0, "../cat_pics/pic3.jpg");
+    System.out.println(testpic.getID());
+    System.out.println(testpic.getFileName());
+  }
+
 }
